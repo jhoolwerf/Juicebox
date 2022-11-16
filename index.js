4f678baba450422b1d747045eff73a15069ec048
Jasmine Hoolwerf <jasmine.hoolwerf@gmail.com>
2022-11-15 21:01:15 -0600

    Added api

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const server = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const apiRouter = require('./api');
+
+const { client } = require('./db');
+client.connect();
+
+server.use(morgan('dev'));
+server.use(bodyParser.json());
+server.use(express.json());
+server.use('/api', apiRouter);
+
+
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log('The server is up on port', PORT)
+});
